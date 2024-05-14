9eaea1e8d439d7c075d737b000e7daaa47d47f63
yhj0619 <114487966+yhj0619@users.noreply.github.com>
2024-05-15 00:32:30 +0900

    first commit

---
+++ src/main/java/com/luckyGirls/forYourNutrition/ForYourNutritionApplication.java
@@ -0,0 +1,13 @@
+package com.luckyGirls.forYourNutrition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForYourNutritionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForYourNutritionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luckyGirls/forYourNutrition/ForYourNutritionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luckyGirls.forYourNutrition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForYourNutritionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
